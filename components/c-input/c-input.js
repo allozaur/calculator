3428976fcf9eea04a9baed1d2ac5e81c61c46fe7
Aleksander Grygier <alek@newfantastic.com>
2021-04-02 01:17:21 +0200

    feat: Finished Mobile UI

--- components/c-input/c-input.js
+++ components/c-input/c-input.js
@@ -1,12 +1,14 @@
 const cInputTemplate = document.createElement('template');
 cInputTemplate.innerHTML = `
   <link rel="stylesheet" href="components/c-input/c-input.css">
-  <input
-    type="number"
-    inputmode="numeric"
-    id="calculator-input"
-    autofocus
-  >
+  <div>
+    <input
+      type="number"
+      inputmode="numeric"
+      id="calculator-input"
+      autofocus
+    >
+  </div
 `;
 
 class cInput extends HTMLElement {
@@ -14,6 +16,21 @@ class cInput extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(cInputTemplate.content.cloneNode(true));
+  }
+
+  focusInput() {
+    let inputEl = this.shadowRoot.querySelector('input')
+    inputEl.focus();
+  }
+
+  connectedCallback() {
+    let inputEl = this.shadowRoot.querySelector('input')
+    inputEl.focus();
+    this.shadowRoot.querySelector('div').addEventListener('click', () => this.focusInput());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('div').removeEventListener();
   }
 }
 
