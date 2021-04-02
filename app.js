211d53f84b62f017c4ae74c5c23c669c52515834
Aleksander Grygier <alek@newfantastic.com>
2021-04-02 13:55:00 +0200

    feat: UI & Logic WIP

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-function calculate() {
-  // event.preventDefault;
-  alert('Leeel');
+function testAlert () {
+  alert('Test')
 }
+
+function inputDigitValue (value) {
+  calculatorInput.value = value
+  console.log(value);
+}
+
+// console.log(calculatorDigit);
+calculatorInput.addEventListener('click', testAlert);
